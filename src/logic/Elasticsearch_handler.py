e56eab58c7b2ac414cbe2478e9a9c396d3102d05
ZKWolf <zkwolf@zkwolf.com>
2023-06-05 23:38:07 +0200

    Old ES_Logger. Will be removed soon...

--- src/logic/Elasticsearch_handler.py
+++ src/logic/Elasticsearch_handler.py
@@ -12,3 +12,17 @@ def es_upload(server, index, log_message):
     response = es.index(index=index, body=data, headers={'Content-Type': 'application/json;charset=UTF-8'})
     document_id = response['_id']
     print("Uploaded dictionary with ID:", document_id)
+
+
+def dev_es_upload(server, index, log_message):
+    if index == "client_event":
+        playerUniqueId = log_message["playerUniqueId"]
+        clientEvent = log_message["clientEvent"]
+        eventType = log_message["eventType"]
+        timestamp = datetime.utcnow().isoformat()
+        upload_string = "Message: {}. Sender: {}.".format(clientEvent, playerUniqueId)
+        upload_message = {"msg_type": "error", "timestamp": timestamp, "message": upload_string, "type": "string"}
+        es = Elasticsearch(server)
+        response = es.index(index="dev_client_event", body=upload_message)
+        document_id = response['_id']
+        print("Uploaded dictionary with ID:", document_id)
