cc4c1c03a874a279cd13d1aa8f4984a07fe97866
ZKWolf <zkwolf@zkwolf.com>
2023-06-08 20:37:03 +0200

    Added logging to mongodb_handler.py and time_handler.py

--- src/logic/time_handler.py
+++ src/logic/time_handler.py
@@ -1,11 +1,17 @@
 import datetime
+import logging
+logger = logging.getLogger(__name__)
 
 
 def get_time():
-    current_datetime = datetime.datetime.now()
-    current_timestamp = int(current_datetime.timestamp())
+    try:
+        current_datetime = datetime.datetime.now()
+        current_timestamp = int(current_datetime.timestamp())
 
-    duration = 86400
-    expiration_timestamp = current_timestamp + duration
+        duration = 86400
+        expiration_timestamp = current_timestamp + duration
 
-    return current_timestamp, expiration_timestamp
+        return current_timestamp, expiration_timestamp
+    except Exception as e:
+        logger.error("Error in time_handler -> " + str(e))
+        return None, None
