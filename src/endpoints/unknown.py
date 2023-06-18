00be32e4d9f9280ae67d7b0ad172833d75616bae
ZKWolf <zkwolf@zkwolf.com>
2023-06-19 00:08:40 +0200

    Fixed catalog maby?

--- src/endpoints/unknown.py
+++ src/endpoints/unknown.py
@@ -1,11 +1,74 @@
 from flask_definitions import *
 
 
-@app.route("/te-18f25613-36778-ue4-374f864b/catalog", methods=["GET"])
-def catalog_get():
+@app.route("/<game_version>/catalog", methods=["GET"])
+def catalog_get(game_version):
+    print("Game Version of Catalog: " + game_version)
     get_remote_ip()
     try:
-        return jsonify({"status": "success"})
+        return jsonify([
+   {
+      "Result":{
+         "Id":"CompositeItemMetadata_0",
+         "DisplayName":"Scavenger_SMOKE_Outfit_STR001_Black_NAME",
+         "Purchasable":True,
+         "Consumable":False,
+         "IsWeapon":False,
+         "InitialQuantity":1,
+         "DefaultCost":{
+            "CurrencyId":3,
+            "Price":10
+         },
+         "EventCostList":{
+            "Name":"Scavenger_SMOKE_Outfit_STR001_Black_NAME",
+            "StartDate":1624038140,
+            "EndDate":1718732540,
+            "Cost":{
+               "CurrencyId":3,
+               "Price":10
+            }
+         },
+         "MetaData":{
+            "GameplayTags":{
+               "TagName":"Class.Runner"
+            },
+            "BundleItems":[
+               "RN_FOG_SMOKE_Head_STR004_Black_Item",
+               "RN_FOG_SMOKE_UpperBody_STR004_Black_Item",
+               "RN_FOG_SMOKE_LowerBody_STR004_Black_Item",
+               "RN_FOG_SMOKE_Vambrace_STR004_Black_Item"
+            ],
+            "RewardBundleItems":[
+               ""
+            ],
+            "RequiredChallangesToComplete":[
+               ""
+            ],
+            "BundlePartOf":"",
+            "PrerequisiteItem":"",
+            "FollowingItem":"",
+            "BundleDiscountPercent":69,
+            "MinPlayerLevel":1,
+            "MinCharacterLevel":1,
+            "Origin":2
+         },
+         "Faction":2,
+         "GameplayTagContainer":{
+            "GameplayTags":{
+               "TagName":"Runner.Smoke"
+            },
+            "ParentTags":{
+               "TagName":"Customization.RN.FullSet"
+            }
+         },
+         "CustomizationGameplayTagByFaction":[
+            2,
+            ""
+         ],
+         "Gender":0
+      }
+   }
+])
     except TimeoutError:
         print("Timeout error")
         return jsonify({"status": "error"})
