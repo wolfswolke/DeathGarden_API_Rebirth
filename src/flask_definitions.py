47f0e602519fbbbb18d57bec65f9a629d4058570
ZKWolf <zkwolf@zkwolf.com>
2024-02-07 23:07:10 +0100

    Added Discord Logic

--- src/flask_definitions.py
+++ src/flask_definitions.py
@@ -5,6 +5,7 @@ from logic.global_handlers import session_manager
 from logic.global_handlers import check_for_game_client
 from logic.global_handlers import sanitize_input
 from logic.mongodb_handler import mongo
+from logic.webhook_handler import discord_webhook
 import json
 import os
 
@@ -18,3 +19,7 @@ mongo_host = config['mongodb']['host']
 mongo_db = config['mongodb']['db']
 mongo_collection = config['mongodb']['collection']
 allowed_tokens = config['api']['allowed_tokens']
+discord_mm_url = config['webhooks']['discord']['mm_url']
+discord_mm_use = config['webhooks']['discord']['mm_use']
+discord_public_url = config['webhooks']['discord']['public_url']
+discord_rebirth_url = config['webhooks']['discord']['rebirth_url']
