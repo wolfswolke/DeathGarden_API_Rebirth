f3be56ca1b4c4e6c954441ecdb1955219c9e05fe
ZKWolf <34097612+wolfswolke@users.noreply.github.com>
2024-04-25 01:38:28 +0200

    Added function from challenge handler

--- src/flask_definitions.py
+++ src/flask_definitions.py
@@ -9,7 +9,7 @@ from logic.webhook_handler import discord_webhook
 from logic.time_handler import get_lifetime
 # from logic.challenge_handler import get_progression_batch, update_progression_batch, get_time_based_challenges
 from logic.hash_handler import hash_handler
-from logic.challenge_handler_new import new_challenge_handler
+from logic.challenge_handler_new import new_challenge_handler, get_challenge_ids_from_inventory
 import json
 import os
 
