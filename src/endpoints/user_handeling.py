ecf8ed5daf0e2b197e14efccc8e0e3c1b370ea91
ZKWolf <34097612+wolfswolke@users.noreply.github.com>
2024-07-21 00:25:41 +0200

    Removed softlaunch kick from dev env

--- src/endpoints/user_handeling.py
+++ src/endpoints/user_handeling.py
@@ -21,10 +21,17 @@ def steam_login_function():
             response = requests.get(
                 'https://api.steampowered.com/ISteamUserAuth/AuthenticateUserTicket/v1/?key={}&ticket={}&appid={}'.format(
                     steam_api_key, steam_session_token, appid))
-            steamid = response.json()["response"]["params"]["steamid"]
-            logger.graylog_logger(level="info", handler="steam_login",
-                                  message="User {} logged in with SOFTLAUNCH".format(steamid))
-            return jsonify({"status": "error"})
+            if dev_env == "false":
+                steamid = response.json()["response"]["params"]["steamid"]
+                logger.graylog_logger(level="info", handler="steam_login",
+                                      message="User {} logged in with SOFTLAUNCH".format(steamid))
+                return jsonify({"status": "error"})
+            if response.json() == {"response": {"error": {"errorcode": 102, "errordesc": "Ticket for other app"}}}:
+                if dev_env == "true":
+                    appid = 480
+                    response = requests.get(
+                        'https://api.steampowered.com/ISteamUserAuth/AuthenticateUserTicket/v1/?key={}&ticket={}&appid={}'.format(
+                            steam_api_key, steam_session_token, appid))
 
         steamid = response.json()["response"]["params"]["steamid"]
         userid, token = mongo.user_db_handler(steamid)
@@ -1477,6 +1484,13 @@ def moderation_check_chat():
         userid = sanitize_input(data["userId"])
         language = sanitize_input(data["language"])
         message = sanitize_input(data["message"])
+        steamid = mongo.get_data_with_list(login=userid, login_steam=False, items={"steamid"})["steamid"]
+        logger.graylog_logger(level="info",
+                              handler="moderation_check_chat",
+                              message=f"User: {userid} | "
+                                      f"SteamID: {steamid} | "
+                                      f"Message: {message} | "
+                                      f"Language: {language}")
         return jsonify({"status": "success", "result": "OK"})
     except TimeoutError:
         return jsonify({"status": "error"})
